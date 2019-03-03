23594fae222c436b81b77612dcceaa649c763570
rleongPd5 <rleong00@stuy.edu>
2019-03-02 22:44:05 -0500

    Added msg for when args length < 1

--- Driver.java
+++ Driver.java
@@ -3,16 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Driver{
     public static void main(String[]args){
-      String filename = args[0];
-      try{
-        Maze f;
-        f = new Maze(filename);//true animates the maze.
-        System.out.println(f);
-        f.setAnimate(true);
-        System.out.println("Solution: "+f.solve());
-        System.out.println(f);
-      }catch(FileNotFoundException e){
-        System.out.println("Invalid filename: "+filename);
+      if (args.length>0){
+        String filename = args[0];
+        try{
+          Maze f;
+          f = new Maze(filename);//true animates the maze.
+          System.out.println(f);
+          f.setAnimate(true);
+          System.out.println("Solution: "+f.solve());
+          System.out.println(f);
+        }catch(FileNotFoundException e){
+          System.out.println("Invalid filename: "+filename);
+        }
+      }
+      else{
+        System.out.println("Please type a file name");
       }
     }
   }
